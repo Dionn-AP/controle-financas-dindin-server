f8f67962d71d7efd4347fb89fba83968f40bdc60
Dionnatan Alves <dionn_a.p@hotmail.com>
2023-02-12 14:39:10 -0300

    doc: nova função de exclusão de categorias

--- src/controladores/categorias.js
+++ src/controladores/categorias.js
@@ -36,31 +36,77 @@ const adicionarCategoria = async (req, res) => {
     }
 
     if (typeof nome != 'string') {
-        return res.status(500).json({ mensagem: "O nome da categoria não pode ser um número" });
+        return res.status(400).json({ mensagem: "O nome da categoria não pode ser um número" });
     }
 
     if (typeof descricao != 'string') {
-        return res.status(500).json({ mensagem: "O descrição da categoria não pode ser um número" });
+        return res.status(400).json({ mensagem: "O descrição da categoria não pode ser um número" });
     }
 
     try {
         const novaCategoria = await knex('categorias').insert({
             nome,
-            descricao: descricao ? descricao : ""
+            descricao
         }).returning('*');
 
         if (!novaCategoria) {
-            return res.status(400).json({ mensagem: "Não foi possível cadastrar a categoria" });
+            return res.status(422).json({ mensagem: "Não foi possível cadastrar a categoria" });
         }
 
         return res.status(201).json({ message: "Nova categoria cadastrada com sucesso" });
     } catch (error) {
         return res.status(400).json(error.message);
     }
+}
 
+const deletarCategoria = async (req, res) => {
+    const { usuario } = req;
+    const { id: idCategoria } = req.params;
+
+    const descricao = [];
+
+    if (!idCategoria) {
+        return res.status(422).json({ mensagem: "Nenhuma categoria foi selecionada" });
+    }
+
+    try {
+        const transacoes = await knex('transacoes')
+            .where({ usuario_id: usuario.id });
+
+        const categoriaVinculada = transacoes.filter((transacao => {
+            return transacao.categoria_id === parseInt(idCategoria)
+        }));
+
+        if (categoriaVinculada.length) {
+            for (const categoria of categoriaVinculada) {
+                descricao.push(categoria)
+            }
+            if (descricao.length === 1) {
+                return res.status(422).json({
+                    mensagem: "A categoria selecionada não pode ser excluída, pois possui vínculo com a seguinte transação", descricao
+                });
+            }
+            return res.status(422).json({
+                mensagem: "A categoria selecionada não pode ser excluída, pois possui vínculo com as seguintes transações", descricao
+            });
+        }
+        const categoriaExcluida = await knex('categorias').
+            where({ id: idCategoria })
+            .del();
+
+        if (!categoriaExcluida) {
+            return res.status(400).json({ "mensagem": "A categoria não foi excluída" });
+        }
+
+        return res.status(200).json({ mensagem: "Categoria excluída com sucesso." });
+
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
 }
 
 module.exports = {
     listarCategoria,
-    adicionarCategoria
+    adicionarCategoria,
+    deletarCategoria
 };
