129d5a3d7a63904cd614c97269aceed1ba677560
Dionnatan Alves <dionn_a.p@hotmail.com>
2023-02-12 12:22:12 -0300

    doc: criando nova função de cadastro de categoria

--- src/controladores/categorias.js
+++ src/controladores/categorias.js
@@ -1,7 +1,7 @@
 const knex = require('../conexao');
 
 const listarCategoria = async (req, res) => {
-    
+
     try {
         const categorias = await knex('categorias').returning("*");
 
@@ -14,8 +14,53 @@ const listarCategoria = async (req, res) => {
     }
 };
 
+const adicionarCategoria = async (req, res) => {
+    const { nome, descricao } = req.body;
 
+    if (!nome && descricao) {
+        return res.status(400).json({
+            mensagem: "Você precisa informar o nome da categoria"
+        });
+    }
 
-module.exports ={
-    listarCategoria
+    if (nome && !descricao) {
+        return res.status(400).json({
+            mensagem: "Você precisa informar uma descrição para a categoria"
+        });
+    }
+
+    if (!nome || !descricao) {
+        return res.status(400).json({
+            mensagem: "Você precisa informar os dados obrigatórios"
+        });
+    }
+
+    if (typeof nome != 'string') {
+        return res.status(500).json({ mensagem: "O nome da categoria não pode ser um número" });
+    }
+
+    if (typeof descricao != 'string') {
+        return res.status(500).json({ mensagem: "O descrição da categoria não pode ser um número" });
+    }
+
+    try {
+        const novaCategoria = await knex('categorias').insert({
+            nome,
+            descricao: descricao ? descricao : ""
+        }).returning('*');
+
+        if (!novaCategoria) {
+            return res.status(400).json({ mensagem: "Não foi possível cadastrar a categoria" });
+        }
+
+        return res.status(201).json({ message: "Nova categoria cadastrada com sucesso" });
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+
+}
+
+module.exports = {
+    listarCategoria,
+    adicionarCategoria
 };
