00a1b42168a2ef5ffb1c47a28fd661317b29117e
Dion <dionn_a.p@hotmail.com>
2024-04-18 07:21:14 -0300

    update: query sql

--- query.sql
+++ query.sql
@@ -1,8 +1,4 @@
-create database dindin;
-
-
---CRIACAO DA TABELA USUARIOS
-drop table if exists usuarios;
+--CRIACAO DA TABELA CATEGORIAS
 
 create table usuarios(
     id serial primary key,
@@ -11,16 +7,16 @@ create table usuarios(
     senha text not null
 );
 
-
 --CRIACAO DA TABELA CATEGORIAS
 drop table if exists categorias;
 
 create table categorias (
     id serial primary key,
     nome varchar(30) not null,
-    descricao text not null
+    descricao text not null,
+    usuario_id integer,
+    foreign key (usuario_id) references usuarios (id)
 );
-
 
 --CRIACAO DA TABELA TRANSACOES
 drop table if exists transacoes;
@@ -36,19 +32,3 @@ create table transacoes (
     foreign key (usuario_id) references usuarios (id),
     tipo varchar(8) not null
 );
-
-
---CRIACAO DOS TIPOS DE CATEGORIAS
-insert into categorias 
-(nome, descricao) 
-values 
-('Salario', 'Receita líquida recebida por trabalho no regime CLT.'), 
-('Alimentação', 'Gêneros alimentícios como frutas, legumes, proteínas e/ou derivados de origem animal ou vegetal.'), 
-('Cuidados Pessoais', 'Produtos de higiene pessoal, perfumes, hidratantes, gel capilar, creme, barbeador, etc.'), 
-('Educacão', 'Escola, reforço escolar, curso profissionalizante, curso técnico, faculdade, universidade.'),
-('Saúde', 'Plano de saúde, compra de remédios e gastos com consultas e exames.'), 
-('Lazer', 'Passeios e atividade que incluem diversão, bem como viagens familiares ou individual, entre outros hobbies.'), 
-('Casa', 'Eletrodomésticos, móveis, utensílios de cama, mesa e banho, ornamento e estética do lar.'), 
-('Assinaturas e Serviços', 'Serviçõs de streaming, internet, telefone, etc.'),
-('Outras receitas', 'Quaisquer outros tipos de receitas.'),
-('Outras despesas', 'Quaisquer outros tipos de despesas.');
